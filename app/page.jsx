e43e2369b72e68bfe620bb2cd0bc4611bc28dfd7
julsamson <jaymsab170@gmail.com>
2023-11-11 19:02:46 +0300

    button styles animation

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import LoginPage from './dashboard/login/page'
 
 export default function Home() {
   return (
@@ -16,10 +18,31 @@ export default function Home() {
 
     <div className='flex justify-center items-center h-screen bg-gray-700'>
       <div>
-      <span className="relative flex h-64 w-64 justify-center items-center">
-        <span className="animate-ping absolute inline-flex h-40 w-40 rounded-3xl bg-white opacity-100"></span>
-        <span className="relative inline-flex rounded-md h-64 w-64 bg-sky-500 bg-[url('/image/julian.jpg')] bg-center bg-cover"></span>
-      </span>
+        <span className="relative flex h-64 w-64 justify-center items-center">
+          <span className="animate-ping absolute inline-flex h-40 w-40 rounded-3xl bg-gray-400 opacity-100"></span>
+          <span className="relative inline-flex rounded-md h-64 w-64 bg-sky-500 bg-[url('/image/julian.jpg')] bg-center bg-cover text-red-500 items-center justify-center">
+            <span className='block space-y-10 '>
+              <h2 className='p-2 rounded-lg cursor-pointer'>
+                <Link href="/" className="animated-button1 rounded-lg">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  Owner
+                </Link>
+              </h2>
+              <h2 className='p-2 rounded-lg cursor-pointer'>
+                <Link href="/" className="animated-button1 rounded-lg">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  client
+                </Link>
+              </h2>
+            </span>
+          </span>
+        </span>
       </div>
     </div>
   )
