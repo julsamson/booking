7170191f811eeb9ff8fc39479b4ebda052650651
julsamson <jaymsab170@gmail.com>
2023-11-16 23:18:05 +0300

    adding postgres.js

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -14,41 +14,41 @@ export default function LoginPage() {
   })
 
   const onLogin = async () => {
-
   }
 
   return (
 
-    <div className='flex flex-col items-center justify-center min-h-screen py-2 text-white'>
-      <h1>Login</h1>
-      <hr />
-      <label htmlFor="email">Email:</label>
-      <input
-        className='p-2 rounded-lg border border-gray-300 mb-4 focus:outline-none focus:border-gray-600'
-        type="email"
-        id='email'
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        placeholder='Email'
-        required
-      />
+    <div>
+      <form onSubmit={onLogin} className="flex flex-col items-center justify-center min-h-screen py-2 text-white">
+        <h1>Login</h1>
+        <hr />
+        <label htmlFor="email">Email:</label>
+        <input
+          className='p-2 rounded-lg border border-gray-300 mb-4 focus:outline-none focus:border-gray-600'
+          type="email"
+          id='email'
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder='Email'
+          required
+        />
 
-      <label htmlFor="password">Password:</label>
-      <input
-        className='p-2 rounded-lg border border-gray-300 mb-4 focus:outline-none focus:border-gray-600'
-        type="password"
-        id='password'
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder='Password'
-        required
-      />
+        <label htmlFor="password">Password:</label>
+        <input
+          className='p-2 rounded-lg border border-gray-300 mb-4 focus:outline-none focus:border-gray-600 text-black'
+          type="password"
+          id='password'
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder='Password'
+          required
+        />
 
-      <button onClick={onLogin} className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'>
-        Login
-      </button>
-      <Link href="/signup">visit Signup page</Link>
-
+        <button type='submit' className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'>
+          Login
+        </button>
+        <Link href="/signup">visit Signup page</Link>
+      </form>
     </div>
   )
 }
--- app/postgress/postgres.js
+++ app/postgress/postgres.js
@@ -1,24 +1,25 @@
-import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
-import { Client } from "@vercel/postgres";
+import { Pool } from "pg";
+import { client } from '@vercel/postgres'
+
+
+const pool = new Pool ({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    connectionString: process.env.POSTGRES_URL,
+})
 
 export async function connect() {
     try {
-
-        const client = new Client({
-            connectionString: process.env.POSTGRES_URL,
-            // Other connection options if needed
-        });
-
-        await client.connect();
-
-        console.log("Connected to PostgreSQL database");
-
-        // You can return the connected client if you want to use it in other parts of your code
-        return client;
-
+      const client = await pool.connect();
+      console.log("Connected to PostgreSQL database");
+  
+      // You can return the connected client if you want to use it in other parts of your code
+      return client;
     } catch (error) {
-        console.log('something went wrong');
-        console.log(error);
+      console.log("Something went wrong");
+      console.error(error);
+      throw error; // Re-throw the error to be caught by the calling function
     }
-}
+  }
