288435d6db7894eb7242d304fc75fd13dfb9d639
julsamson <jaymsab170@gmail.com>
2023-11-22 13:35:11 +0300

    commited

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,24 +1,24 @@
 import { NextResponse } from "next/server";
-import { db } from '@/lib/db'
+import prisma from "@/lib/db";
 
-export async function POST(req, res){
-   try {
-    const body = await req.json();
-    const { username, password, email, room} = body;
+export async function POST(req, res) {
+    try {
+        const { username, password, email, room } = body;
+        const body = await req.json();
 
-    //check if the user exist
+        //check if the user exist
 
-    const existingUser = await db.Owner.findUnique({
-        where: { email: email}
-    });
+        const existingUser = await db.Owner.findUnique({
+            where: { email: email }
+        });
 
-    if(existingUser) {
-        return NextResponse.json({ user: null, message: "user with that email already exits"}, { status: 409 })
+        if (existingUser) {
+            return NextResponse.json({ user: null, message: "user with that email already exits" }, { status: 409 })
+        }
+
+
+        return NextResponse.json(body)
+    } catch (error) {
+
     }
-
-
-    return NextResponse.json(body)
-   } catch (error) {
-    
-   }
 }
